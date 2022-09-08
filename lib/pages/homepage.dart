1c2a39bd35c7f7b5d64517230db62db07e4b619c
julio <jcso2@aluno.ifal.edu.br>
2022-09-08 15:45:55 -0300

    primeiro commit

---
+++ lib/pages/homepage.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => HhomePageState();
+}
+
+class HhomePageState extends State<HomePage> {
+
+  String _search = "";
+  int _offSet = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.black,
+          title: Image.asset("images/title.gif"),
+          centerTitle: true,
+        ),
+        backgroundColor: Colors.black,
+        body: Column(
+          children: [
+            Padding(
+              padding: EdgeInsets.all(20),
+              child: Column(
+                children: [
+                  TextField(
+                    decoration: const InputDecoration(
+                      labelText: 'Pesquise aqui!',
+                      labelStyle: TextStyle(color: Colors.white),
+                      // enabledBorder habilita algumas configurações da borda
+                      enabledBorder: const OutlineInputBorder(
+                          borderSide: BorderSide(
+                        color: Colors.white,
+                        width: 2,
+                      )),
+                    ),
+                    style: const TextStyle(
+                      color: Colors.white,
+                      fontSize: 18
+                    ),
+                    textAlign: TextAlign.center,
+                    //s
+                    onSubmitted: (text){
+                      _search = text;
+                      _offSet = 0;
+
+                    },
+                  )
+                ],
+              ),
+            )
+          ],
+        ));
+  }
+}
